09a9e70e4802ebb6d992c4ce25acd54ee1d49da2
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2024-10-12 14:54:29 +0530

    Created a function that print the sum of the nth number

--- Day4/index.cpp
+++ Day4/index.cpp
@@ -1,66 +1,20 @@
 #include <iostream>
 using namespace std;
+
+int sum(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
 int main()
 {
     int n;
     cin >> n;
-
-    // 1 23 456
-
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = i + 1; j > 0; j--)
-    //     {
-    //         cout << j << " ";
-    //     }
-    //     cout << endl;
-    // }
-
-    // A BC DEF
-    // char ch = 'A';
-
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = i + 1; j > 0; j--)
-    //     {
-    //         cout << ch << " ";
-    //         ch += 1;
-    //     }
-    //     cout << endl;
-    // }
-
-    // 1111 222 33 4
-
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = 0; j < i; j++)
-    //     {
-    //         cout << " ";
-    //     }
-
-    //     for (int j = 0; j < n - i; j++)
-    //     {
-    //         cout << i + 1;
-    //     }
-    //     cout << endl;
-    // }
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            cout << " ";
-        }
-        for (int j = 0; j <= i; j++)
-        {
-            cout << j + 1;
-        }
-
-        for (int j = i; j > 0; j--)
-        {
-            cout << j;
-        }
-
-        cout << endl;
-    }
+    cout << "Sum = " << sum(n);
+    return 0;
 }
